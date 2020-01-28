0fa41d1e958eb3c69a7f905cc785a8b6117edf1a
Yuan <804547696@qq.com>
2020-01-28 13:42:08 +0800

    Add Populaiton Groups

--- Chapter16/world_population.py
+++ Chapter16/world_population.py
@@ -11,6 +11,7 @@ with open(filename, 'r') as file_object:
 	pop_data = json.load(file_object)
 
 country_population = {}
+
 #打印每个国家2010年的人口数量
 for pop_dict in pop_data:
 	if pop_dict['Year'] == '2010':
@@ -19,9 +20,24 @@ for pop_dict in pop_data:
 		code = get_country_code(country_name)
 		if code:
 			country_population[code] = population
-			
+
+#根据人口数量将所有国家分为三组
+country_pops_1, country_pops_2, country_pops_3 = {}, {} ,{}
+for country, population in country_population.items():
+	if population < 1e7:
+		country_pops_1[country] = population
+	elif population < 1e9:
+		country_pops_2[country] = population
+	else:
+		country_pops_3[country] = population
+
+#看看每组分别包含多少个国家
+print(len(country_pops_1), len(country_pops_2), len(country_pops_3))
+
 wm = World()
 wm.title = "World Population in 2010, by Country"
-wm.add('2010', country_population)
+wm.add('0-10m', country_pops_1)
+wm.add('10-100m', country_pops_2)
+wm.add('>1bn', country_pops_3)
 
 wm.render_to_file("World_population.svg")
