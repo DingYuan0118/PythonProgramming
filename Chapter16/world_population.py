d14b77760b395a36dfff792163c78678e18de734
unknown <804547696@qq.com>
2020-01-28 13:05:40 +0800

    Represent World Populaiton Map

--- Chapter16/world_population.py
+++ Chapter16/world_population.py
@@ -1,14 +1,27 @@
 import json
+import pygal
+from pygal.maps.world import World
 
+from country_codes import get_country_code
 #将数据加载到一个列表中
+
 filename = 'population_data.json'
 
 with open(filename, 'r') as file_object:
 	pop_data = json.load(file_object)
-	
+
+country_population = {}
 #打印每个国家2010年的人口数量
 for pop_dict in pop_data:
 	if pop_dict['Year'] == '2010':
 		country_name = pop_dict['Country Name']
-		population = pop_dict['Value']
-		print(country_name + ": " + population)
+		population = int(float(pop_dict['Value']))#转为浮点数在转为整数
+		code = get_country_code(country_name)
+		if code:
+			country_population[code] = population
+			
+wm = World()
+wm.title = "World Population in 2010, by Country"
+wm.add('2010', country_population)
+
+wm.render_to_file("World_population.svg")
