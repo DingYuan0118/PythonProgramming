e86e4267a65649aafd2815470381cf4b262694b9
Yuan <804547696@qq.com>
2020-01-28 18:25:03 +0800

    Use Light Color

--- Chapter16/world_population.py
+++ Chapter16/world_population.py
@@ -2,7 +2,7 @@ import json
 
 import pygal
 from pygal.maps.world import World
-from pygal.style import RotateStyle
+from pygal.style import RotateStyle, LightColorizedStyle
 
 from country_codes import get_country_code
 #将数据加载到一个列表中
@@ -37,11 +37,11 @@ for country, population in country_population.items():
 print(len(country_pops_1), len(country_pops_2), len(country_pops_3))
 
 
-wm_style =RotateStyle('#336699')
+wm_style =RotateStyle('#336699', base_style=LightColorizedStyle)
 wm = World(style=wm_style)
 wm.title = "World Population in 2010, by Country"
 wm.add('0-10m', country_pops_1)
 wm.add('10-100m', country_pops_2)
 wm.add('>1bn', country_pops_3)
 
-wm.render_to_file("World_population.svg")
+wm.render_to_file("World_population_LightColorized.svg")
