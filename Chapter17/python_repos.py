afd3b1a2a1347678061ed575cb2c908f6ab8fd6b
Yuan <804547696@qq.com>
2020-01-29 17:47:53 +0800

    Use Web API

---
+++ Chapter17/python_repos.py
@@ -0,0 +1,25 @@
+import requests
+
+#执行API调用并存储响应
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print("Status Code:", r.status_code)
+
+#将API响应存储在一个变量中
+response_dict = r.json()
+print("Total repositories: ", response_dict['total_count'])
+
+#探索有光仓库的信息
+repo_dicts = response_dict['items']
+print("Repositories returned: ", len(repo_dicts))
+
+#研究第一个仓库
+for repo_dict in repo_dicts:
+	print("\nSelected information about first repo:")
+	print('Name:', repo_dict['name'])
+	print('Owner:', repo_dict['owner']['login'])
+	print('Stars:', repo_dict['stargazers_count'])
+	print('Repository:', repo_dict['html_url'])
+	print('Created:', repo_dict['created_at'])
+	print('Updated:', repo_dict['updated_at'])
+	print('Description:', repo_dict['description'])
